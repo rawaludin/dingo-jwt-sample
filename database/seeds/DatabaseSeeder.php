24d543c776c21a0f5d1aa2a2ebc1bf712423138e
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2015-08-22 20:43:51 +0700

    initial

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        DB::table('users')->delete();
+
+        $users = array(
+                ['name' => 'Ryan Chenkie', 'email' => 'ryanchenkie@gmail.com', 'password' => Hash::make('secret')],
+                ['name' => 'Chris Sevilleja', 'email' => 'chris@scotch.io', 'password' => Hash::make('secret')],
+                ['name' => 'Holly Lloyd', 'email' => 'holly@scotch.io', 'password' => Hash::make('secret')],
+                ['name' => 'Adnan Kukic', 'email' => 'adnan@scotch.io', 'password' => Hash::make('secret')],
+        );
+
+        // Loop through each user above and create the record for them in the database
+        foreach ($users as $user)
+        {
+            \App\User::create($user);
+        }
+
+        Model::reguard();
+    }
+}
---
+++ public/scripts/app.js
@@ -0,0 +1,31 @@
+// public/scripts/app.js
+
+(function() {
+
+    'use strict';
+
+    angular
+        .module('authApp', ['ui.router', 'satellizer'])
+        .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+
+            // Satellizer configuration that specifies which API
+            // route the JWT should be retrieved from
+            $authProvider.loginUrl = '/api/authenticate';
+
+            // Redirect to the auth state if any other states
+            // are requested other than users
+            $urlRouterProvider.otherwise('/auth');
+
+            $stateProvider
+                .state('auth', {
+                    url: '/auth',
+                    templateUrl: '../views/authView.html',
+                    controller: 'AuthController as auth'
+                })
+                .state('users', {
+                    url: '/users',
+                    templateUrl: '../views/userView.html',
+                    controller: 'UserController as user'
+                });
+        });
+})();
